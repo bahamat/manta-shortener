85ce7cd9ff16c4a2d8f929f664542350acbf5f4c
Brian Bennett <brian.bennett@joyent.com>
2017-11-26 12:11:08 -0800

    manta-setup - switch to raw cloudapi, add usage, fix rule syntax

--- tools/manta-setup.sh
+++ tools/manta-setup.sh
@@ -7,7 +7,16 @@ if [[ -n "$TRACE" ]]; then
     set -o xtrace
 fi
 
-while getopts 'l:e:p:p:d:' options; do
+usage() {
+    printf '%s -l login -e email -P triton-profile -d manta_directory [ -p password ]\n' "$0"
+    printf '\t-l login\tsub-user login name\n'
+    printf '\t-e email\tsub-user email address\n'
+    printf '\t-P profile\tnode-triton profile name (default is env)\n'
+    printf '\t-d directory\tmanta directory for persistent storage\n'
+    printf '\t-p password\tsub-user password (random if empty)\n'
+    exit "$1"
+}
+while getopts 'l:e:p:p:d:h' options; do
     case $options in
         l ) login="${OPTARG}";;
         e ) email="${OPTARG}";;
@@ -28,11 +37,11 @@ fi
 user_json=$(printf '{"login":"%s","email":"%s","password":"%s"}' "$login" "$email" "$password")
 role_json=$(printf '{"name":"manta_shortener","members":["%s"],"policies":["manta_shortener"]}' "$login")
 
-policy_json='{"name":"manta_shortener","rules":["CANgetdirectory","CANgetobject","CANputdirectory","CANputobject"]}'
+policy_json='{"name":"manta_shortener","rules":["CAN getdirectory","CAN getobject","CAN putdirectory","CAN putobject"]}'
 
-triton "${profile[@]}" rbac user --add <(printf '%s' "$user_json")
-triton "${profile[@]}" rbac policy add <(printf '%s' "$policy_json")
-triton "${profile[@]}" rbac role add <(printf '%s' "$role_json")
+triton "${profile[@]}" cloudapi /my/users -X POST -d "$user_json"
+triton "${profile[@]}" cloudapi /my/policies -X POST -d "$policy_json"
+triton "${profile[@]}" cloudapi /my/roles -X POST -d "$role_json"
 
 mmkdir "$directory"
 mchmod +manta_shortener "$directory"
